fbdb5d15dfed2643e97a39c064e3cdb5930bc6e8
carlhopf <carlhopf.com@gmail.com>
2023-03-17 11:28:30 +0000

    remaining FT.* commands - set FIRST_KEY_INDEX to 1

--- packages/search/lib/commands/DICTADD.ts
+++ packages/search/lib/commands/DICTADD.ts
@@ -1,6 +1,8 @@
 import { RedisCommandArguments } from '@redis/client/dist/lib/commands';
 import { pushVerdictArguments } from '@redis/client/dist/lib/commands/generic-transformers';
 
+export const FIRST_KEY_INDEX = 1;
+
 export function transformArguments(dictionary: string, term: string | Array<string>): RedisCommandArguments {
     return pushVerdictArguments(['FT.DICTADD', dictionary], term);
 }
